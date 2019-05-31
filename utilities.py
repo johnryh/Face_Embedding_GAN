5ec4766cf0697623944f51cbacd7257133e7f6e2
johnryh <yr41@duke.edu>
2019-05-31 14:49:00 -0400

    updated GNU GPL V2 license

--- utilities.py
+++ utilities.py
@@ -1,3 +1,22 @@
+'''
+
+Copyright 2018 - 2019 Duks University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License Version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License Version 2
+along with this program.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+
+'''
+
+
 from skimage.io import imsave
 import numpy as np
 from config import *
