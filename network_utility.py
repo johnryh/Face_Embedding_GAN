2f8b9df7174e598b2d5e1929f83681bb0f05a16f
Yinhao Ren <ryh199582@gmail.com>
2019-08-13 11:47:47 -0700

    Update network_utility.py

--- network_utility.py
+++ network_utility.py
@@ -1,6 +1,6 @@
 '''
 
-Copyright 2018 - 2019 Duks University
+Copyright 2018 - 2019 Duke University
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License Version 2 as published by
